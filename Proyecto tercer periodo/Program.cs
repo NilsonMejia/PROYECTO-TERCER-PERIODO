96159c277fa5435cc2b531f8376c2e11dc2ac5ff
LUIS DAVID CARBAJAL GUERRA <2023CG602@unicaeselsalvador0.onmicrosoft.com>
2024-05-11 19:01:36 -0600

    Empezando el registro de entrada

--- Proyecto tercer periodo/Program.cs
+++ Proyecto tercer periodo/Program.cs
@@ -2,9 +2,22 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Estacionamiento");
+        int salida, vehiculo, pago;
+        Console.WriteLine("  //////////////////////////////     Estacionamiento     ////////////////////////// ");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Registro de entrada");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Por favor ingrese su vehiculo");
+        Console.WriteLine("1. Carro sedan o camioneta    2. Motocicleta o bicicleta");
+        vehiculo = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
 
-        int salida, vehiculo, pago
+
+
     }
 
 
