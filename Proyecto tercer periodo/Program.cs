9ac976dc4ef737191f26f2c94b1d5eabb035f6a2
EDWIN LEONEL FLORES TREJO <2022FT650@unicaeselsalvador0.onmicrosoft.com>
2024-05-11 19:08:57 -0600

    se agrego la graficadora

--- Proyecto tercer periodo/Program.cs
+++ Proyecto tercer periodo/Program.cs
@@ -16,11 +16,32 @@
         vehiculo = int.Parse(Console.ReadLine());
         Console.WriteLine("");
 
+        int filas = 19;
+        int columnas = 70;
+        switch (vehiculo)
+        {
+            case 1:
+                Console.WriteLine("                           ZONA NORTE");
+                for (int i = 2; i < filas; i++)
+                {
+                    for (int j = 7; j < columnas; j++)
+                    {
+                        if (j == 0 || j == columnas - 1 || i == 0 || i == filas - 9 || j % 7 == 0)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("                              ZONA SUR");
+                Console.WriteLine(" ");
+            break;
 
-
+        }
     }
-
-
-
 }
 
