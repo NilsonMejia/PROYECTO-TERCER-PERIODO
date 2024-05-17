949be502846a7040c5816973da304d66eb160d1f
EDWIN LEONEL FLORES TREJO <2022FT650@unicaeselsalvador0.onmicrosoft.com>
2024-05-16 19:52:02 -0600

    se corrigio los graficadores

--- Proyecto tercer periodo/Program.cs
+++ Proyecto tercer periodo/Program.cs
@@ -4,7 +4,7 @@ class Estacionamineto
 {
     public static void Main(string[] args)
     {
-        int salida, vehiculo, pago, zona, lugar, tarjeta, parqueo1 = 6, parqueo2 = 5, parqueo3 = 9, parqueo4 = 7;
+        int salida, vehiculo, pago, zona, lugar, tarjeta, parqueo;
         double cambio;
         Console.WriteLine("  //////////////////////////////     Estacionamiento     ////////////////////////// ");
         Console.WriteLine("");
@@ -21,206 +21,60 @@ class Estacionamineto
 
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-        int filas = 19;
-        int columnas = 70;
+
         switch (vehiculo)
         {
             case 1:
-                Console.WriteLine("                           ZONA NORTE");
-                for (int i = 2; i < filas; i++)
+                int filas = 4; // cantidad de cuadros en fila
+                int columnas = 16; // cantidad de cuadros en columno
+                int tamañoCuadro = 10; // tamaño de cada cuadro
+
+                int nuevolugar = 1;
+                do
                 {
-                    for (int j = 7; j < columnas; j++)
+                    DibujarCuadroGrande(filas, columnas, tamañoCuadro);
+
+                    static void DibujarCuadroGrande(int filas, int columnas, int tamañoCuadro) // el static void es un tipo de valor devuelto de un metodo que queda estatico
                     {
-                        if (j == 0 || j == columnas - 1 || i == 0 || i == filas - 9 || j % 7 == 0)
+                        int alturaTotal = filas * tamañoCuadro;
+                        int anchuraTotal = columnas * tamañoCuadro;
+
+                        for (int i = 0; i < alturaTotal; i++)
                         {
-                            Console.Write("*");
+                            for (int j = 0; j < anchuraTotal; j++)
+                            {
+                                if (EsBordeCuadro(i, j, tamañoCuadro))
+                                {
+                                    Console.Write("*");
+                                }
+                                else
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
+                            Console.WriteLine();
                         }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
+                        Console.WriteLine(new string('*', anchuraTotal));
                     }
-                    Console.WriteLine();
+
+                    static bool EsBordeCuadro(int i, int j, int tamañoCuadro)
+                    {
+                        return (i % tamañoCuadro == 0 || j % tamañoCuadro == 0 || i % tamañoCuadro == tamañoCuadro - 3 || j % tamañoCuadro == tamañoCuadro - 1);
+                    }
+
+                    //aqui va pegar el siguiente
+                    //se va pegar de desde donde dice "empieza el otro graficador"
+
+                    Console.WriteLine("¿desea cambiar su parqueo?");
+                    Console.WriteLine("1. SI     2. NO");
+                    parqueo = int.Parse(Console.ReadLine());
+                    Console.Clear();
                 }
-                Console.WriteLine("                              ZONA SUR");
-                Console.WriteLine(" ");
-                Console.WriteLine(" ");
-                Console.WriteLine("Por favor selecciones lugar de estacionamiento");
-                Console.WriteLine(" 1. ZONA NORTE      2. ZONA SUR ");
-                zona = int.Parse(Console.ReadLine());
-                Console.Clear();
-                int fil = 1;
-                int column = 70;
-                switch (zona)
-                {
-                    case 1:
-
-                        Console.WriteLine("");
-                        Console.WriteLine("Parqueos disponibles de la zona");
-                        Console.WriteLine("   1      2      3      4      5      6      7     8       9");
-
-                        int filaColoreada = 5;
-                        int columnaColoreadaInicio = 42;
-                        int columnaColoreadaFin = 70;
-                        // Posición de la fila y columna para colorear
-
-                        for (int i = 2; i < filas; i++)
-                        {
-                            for (int j = 7; j < columnas; j++)
-                            {
-                                // Si está en el borde o en la fila/columna coloreada, imprime un asterisco
-                                if (j == 0 || j == column - 1 || i == 18 || i == fil + 0 || j % 7 == 0 || (j >= columnaColoreadaInicio && j <= columnaColoreadaFin && i == filaColoreada))
-                                {
-                                    Console.Write("*");
-                                }
-                                else
-                                {
-                                    Console.Write(" ");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine("Por favor ingrese un luga disponible de la zona");
-                        lugar = int.Parse(Console.ReadLine());
-                        while (parqueo1 <= lugar)
-                        {
-                            Console.WriteLine("Por favor ingrese un parqueo disponible");
-                            lugar = int.Parse(Console.ReadLine());
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("");
-                        Console.WriteLine("Parqueos disponibles de la zona");
-                        int filaColoread = 15;
-                        int columnaColoreadaInici = 2;
-                        int columnaColoreadaFi = 42;
-                        // Posición de la fila y columna para colorear
-
-                        for (int i = 2; i < filas; i++)
-                        {
-                            for (int j = 7; j < columnas; j++)
-                            {
-                                // Si está en el borde o en la fila/columna coloreada, imprime un asterisco
-                                if (j == 1 || j == column - 1 || i == 2 || i == fil - 0 || j % 7 == 0 || (j >= columnaColoreadaInici && j <= columnaColoreadaFi && i == filaColoread))
-                                {
-                                    Console.Write("*");
-                                }
-                                else
-                                {
-                                    Console.Write(" ");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine("   1      2      3      4      5      6      7     8       9");
-                        Console.WriteLine("");
-                        Console.WriteLine("Por favor ingrese un luga disponible de la zona");
-                        lugar = int.Parse(Console.ReadLine());
-                        while (parqueo2 >= lugar)
-                        {
-                            Console.WriteLine("Por favor ingrese un parqueo disponible");
-                            lugar = int.Parse(Console.ReadLine());
-                        }
-                        break;
-                }
+                while (parqueo == nuevolugar);
                 break;
             case 2:
-                int fi = 19;
-                int colu = 69;
-                Console.WriteLine("                           ZONA NORTE");
-                for (int i = 2; i < fi; i++)
-                {
-                    for (int j = 7; j < colu; j++)
-                    {
-                        if (j == 0 || j == colu - 1 || i == 0 || i == fi - 9 || j % 4 == 0)
-                        {
-                            Console.Write("*");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("                              ZONA SUR");
-                Console.WriteLine(" ");
-                Console.WriteLine("Por favor selecciones lugar de estacionamiento");
-                Console.WriteLine(" 1. ZONA NORTE      2. ZONA SUR ");
-                zona = int.Parse(Console.ReadLine());
-                Console.Clear();
-                int fila = 1;
-                int columna = 69;
-                switch (zona)
-                {
-                    case 1:
-                        Console.WriteLine("");
-                        Console.WriteLine("Parqueos disponibles de la zona");
-                        Console.WriteLine("   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");
-                        int filaColoreadaa = 6;
-                        int columnaColoreadaInicioo = 2;
-                        int columnaColoreadaFinn = 44;
-                        for (int i = 2; i < filas; i++)
-                        {
-                            for (int j = 7; j < columnas; j++)
-                            {
-                                if (j == 0 || j == columna - 1 || i == 18 || i == fila + 0 || j % 4 == 0 || (j >= columnaColoreadaInicioo && j <= columnaColoreadaFinn && i == filaColoreadaa))
-                                {
-                                    Console.Write("*");
-                                }
-                                else
-                                {
-                                    Console.Write(" ");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine("Por favor ingrese un luga disponible de la zona");
-                        lugar = int.Parse(Console.ReadLine());
-                        while (parqueo3 >= lugar)
-                        {
-                            Console.WriteLine("Por favor ingrese un parqueo disponible");
-                            lugar = int.Parse(Console.ReadLine());
-                        }
-                        break;
-                    case 2:
-
-                        Console.WriteLine("");
-                        Console.WriteLine("Parqueos disponibles de la zona");
-                        int filaColoreadaaa = 15;
-                        int columnaColoreadaIniciooo = 32;
-                        int columnaColoreadaFinnn = 68;
-                        // Posición de la fila y columna para colorear
-
-                        for (int i = 2; i < filas; i++)
-                        {
-                            for (int j = 7; j < columnas; j++)
-                            {
-                                // Si está en el borde o en la fila/columna coloreada, imprime un asterisco
-                                if (j == 5 || j == columna - 1 || i == 2 || i == fila - 7 || j % 4 == 0 || (j >= columnaColoreadaIniciooo && j <= columnaColoreadaFinnn && i == filaColoreadaaa))
-                                {
-                                    Console.Write("*");
-                                }
-                                else
-                                {
-                                    Console.Write(" ");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine("   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15");
-                        Console.WriteLine("");
-                        Console.WriteLine("Por favor ingrese un luga disponible de la zona");
-                        lugar = int.Parse(Console.ReadLine());
-                        while (parqueo4 <= lugar)
-                        {
-                            Console.WriteLine("Por favor ingrese un parqueo disponible");
-                            lugar = int.Parse(Console.ReadLine());
-                        }
-
-                        break;
-                }
-                break;
+               
+            break;
         }
         Console.Clear();
         Random rnd = new Random();
